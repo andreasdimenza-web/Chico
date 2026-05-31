85bbae59e3d126199dd3ca3c176fac95a2811b30
andreasdimenza-web <andreasdimenza@gmail.com>
2026-05-31 16:40:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,17 +9,17 @@ function add() {
   data[c] = (data[c] || 0) + Number(a);
 
   list.innerHTML = "";
-
-  for (let item of Object.entries(data).sort((a,b)=>b[1]-a[1])) {
+  for (let item of Object.entries(data).sort((a, b) => b[1] - a[1])) {
     list.innerHTML += `<li>${item[0]}: $${item[1]}</li>`;
   }
+
+  let sum = 0;
+  for (let amount of Object.values(data)) {
+    sum += amount;
+  }
+
+  document.getElementById("totalAmount").innerHTML = "Total Spending: $" + sum;
+
+  cat.value = "";
+  amt.value = "";
 }
-
-let sum = 0;
-
-for (let amount of Object.values(data)) {
-  sum += amount;
-}
-
-document.getElementById("totalAmount").innerHTML =
-  "Total Spending: $" + sum;
